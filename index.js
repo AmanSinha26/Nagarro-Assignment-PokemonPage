cc07a2ce42f0a551f30cc5fd9a0738f098ba6e96
AmanSinha26 <aman.sinha26@gmail.com>
2022-07-07 23:46:05 +0530

    pokemon

---
+++ index.js
@@ -0,0 +1,11 @@
+let ashBtn = document.getElementById('ashBtn');
+
+ashBtn.addEventListener("click", function () {
+    window.open('ash.html');
+});
+
+let garyBtn = document.getElementById('garyBtn');
+
+garyBtn.addEventListener("click", function () {
+    window.open('gary.html');
+});
